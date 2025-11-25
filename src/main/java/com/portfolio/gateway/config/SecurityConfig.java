43cfdd991c80af3b2a15a808139f3d79ffacba60
roralo <roberto.rajo.lopez@gmail.com>
2025-11-25 14:05:19 +0100

    fix(cors): allow patch + ng serve origins

--- src/main/java/com/portfolio/gateway/config/SecurityConfig.java
+++ src/main/java/com/portfolio/gateway/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
             HttpMethod.GET.name(),
             HttpMethod.POST.name(),
             HttpMethod.PUT.name(),
+            HttpMethod.PATCH.name(),
             HttpMethod.DELETE.name(),
             HttpMethod.OPTIONS.name()
         ));
