703bebdd1e759d01264ee4a7bf0294e5ded6d860
roralo <roberto.rajo.lopez@gmail.com>
2025-12-01 09:40:04 +0100

    Assert application context in tests

--- src/test/java/com/portfolio/gateway/GatewayServiceApplicationTests.java
+++ src/test/java/com/portfolio/gateway/GatewayServiceApplicationTests.java
@@ -1,12 +1,20 @@
 package com.portfolio.gateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class GatewayServiceApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertThat(applicationContext).as("Gateway context should load").isNotNull();
     }
 }
